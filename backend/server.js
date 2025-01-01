fa0347e5446f8353d70b75dca12cc9d6712e000b
milkeviich <glebmilkevich@icloud.com>
2025-01-01 16:17:06 -0500

    files

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 // Enable CORS for frontend
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL
+  origin: 'http://localhost:5173' || 'https://clamare.store', // Replace with your frontend URL
   credentials: true,
 }));
 
