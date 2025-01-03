4fc0ef975370f4a4f6ef24c41b66c0542834feab
milkeviich <glebmilkevich@icloud.com>
2025-01-03 00:50:30 -0500

    files

--- src/components/ShoppingBag.jsx
+++ src/components/ShoppingBag.jsx
@@ -361,7 +361,7 @@ const ShoppingBag = ({ onCheckout, onClose }) => {
           </div>
           <div style={{
             position: 'sticky',
-            bottom: isSmallScreen ? 'calc(env(safe-area-inset-bottom) + 74)' : 74,
+            bottom: 74,
             padding: '10px 20px',
             backgroundColor: 'var(--main-bg-color)',
             borderTop: '1px solid var(--border-color)',
@@ -413,9 +413,9 @@ const ShoppingBag = ({ onCheckout, onClose }) => {
           </div>
           <div style={{
             position: 'sticky',
-            bottom: 0,
+            bottom: isSmallScreen ? 'env(safe-area-inset-bottom)' : 0,
             padding: '20px',
-            paddingBottom: isSmallScreen ? 'env(safe-area-inset-bottom)' : '20px',
+            paddingBottom: '20px',
             backgroundColor: 'var(--main-bg-color)',
             borderTop: '1px solid var(--border-color)',
             marginTop: -12
