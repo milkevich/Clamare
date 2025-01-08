35c32990e2635ac7ecb6a93bdca5f49ec103e5a9
milkeviich <glebmilkevich@icloud.com>
2025-01-08 18:13:12 -0500

    files

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -20,7 +20,7 @@ class ErrorBoundary extends Component {
         <div style={{height: 'calc(95vh - 2.5rem)', width: 'calc(100vw - 2.5rem)', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '1.25rem'}}>
           <div style={{fontSize: '12px', display: 'flex', flexDirection: 'column', gap: '0.5rem', textAlign: 'center', maxWidth: '400px', margin: 'auto'}}>
           <p style={{margin: 0}}>CLAMÁRE:</p>
-          <p style={{margin: 0}}>SOMETHING WENT WRONG. PLEASE TRY AGAIN OR CONTACT SUPPORT AT <a href='https://mail.google.com/mail/u/0/#inbox?compose=jrjtWvPfnzRbwkwVkzzFQGHxvtJmkwSnGvdshwRqDknzSrmCPGtGRMHLFKjWlvwcmZhPsrhQ'>SUPPORT@CLAMARE.STORE</a></p>
+          <p style={{margin: 0}}>SOMETHING WENT WRONG. PLEASE TRY AGAIN OR CONTACT SUPPORT AT <a href='mailto:support@clamare.store' style={{color: 'var(--main-color)', fontWeight: '600', textDecoration: 'none'}}>SUPPORT@CLAMARE.STORE</a></p>
           </div>
         </div>
       )
